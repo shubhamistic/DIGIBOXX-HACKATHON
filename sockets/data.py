6b1a1251d8da3d45c1dc24d5b3fcd2335331790b
shubhamistic <shubham2003garg@gmail.com>
2024-06-02 09:50:14 +0530

    update data sockets

--- sockets/data.py
+++ sockets/data.py
@@ -19,12 +19,14 @@ class DataNamespace(Namespace):
             # decode the jwt token and get the user Id
             user_id = jwt_utils.get_jwt_identity(jwt_token)
 
-            if user_id:
-                # add the socket id to the user room
-                data_socket_manager.add_socket_to_room(
-                    user_id=user_id,
-                    socket_id=request.sid
-                )
+            # add the socket id to the user room
+            data_socket_manager.add_socket_to_room(
+                user_id=user_id,
+                socket_id=request.sid
+            )
+
+            # emit the data
+            data_socket_manager.emit_data_refreshed_event(user_id=user_id)
 
     def on_disconnect(self):
         global data_socket_manager
